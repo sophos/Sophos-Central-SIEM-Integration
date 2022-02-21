ac50c0a3f6c228419f885622d4e5362ebca5557b
ruhulamini.khatri <ruhulamini.khatri@sophos.com>
2022-02-21 16:40:45 +0530

    added append_nul

--- api_client.py
+++ api_client.py
@@ -160,6 +160,7 @@ class ApiClient:
             logging_handler = logging.FileHandler(
                 os.path.join(logdir, self.config.filename), "a", encoding="utf-8"
             )
+        logging_handler.append_nul = self.config.append_nul == "true"
         SIEM_LOGGER.addHandler(logging_handler)
 
     def get_past_datetime(self, hours):
